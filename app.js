b8aba1e228d1bc85e0e03cac8e4fbe43fb8c645e
Aleksis Sideris <Aleksis Sideris>
2016-11-30 13:07:10 +0200

    innitial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+var urls = ['http://www.laughfactory.com/jokes/joke-of-the-day']
+
+setInterval(function() {
+	
+	urls.map(function(url){
+			request(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body) 
+			}
+		});
+	});
+}, 15 * 1000 * 60) // 15 mins -> 15 * 1000 * 60 = 900000
+
+app.listen(3001);
+
